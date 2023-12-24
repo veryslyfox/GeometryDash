5741ba30c040d9db7f3684eb02239d97c6a21895
Avgust Zagorodnikov <zagavg@yandex.ru>
2023-12-24 16:53:40 +0300

    Update

---
+++ Graphic.cs
@@ -0,0 +1,112 @@
+using System.Drawing;
+using System.Windows.Media.Imaging;
+
+struct Rectangle
+{
+    public static Rectangle Create(int x, int y, int width, int height)
+    {
+        return new(x, y, x + width, y + height);
+    }
+    public Rectangle(int x1, int x2, int y1, int y2)
+    {
+        X1 = x1;
+        X2 = x2;
+        Y1 = y1;
+        Y2 = y2;
+    }
+
+    public int X1 { get; }
+    public int Y1 { get; }
+    public int Width { get; }
+    public int Height { get; }
+    public int X2 { get; }
+    public int Y2 { get; }
+    public static Rectangle? operator &(Rectangle left, Rectangle right)
+    {
+        var x1 = 0;
+        var x2 = 0;
+        var v1 = left.X1 < right.X2;
+        var v2 = left.X2 < right.X1;
+        if (v1)
+        {
+            if (v2)
+            {
+                return null;
+            }
+            else
+            {
+                x1 = right.X2;
+                x2 = left.X1;
+            }
+        }
+        else
+        {
+            if (v2)
+            {
+                x1 = left.X1;
+                x2 = right.X2;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        var y1 = 0;
+        var y2 = 0;
+        var v3 = left.X1 < right.X2;
+        var v4 = left.X2 < right.X1;
+        if (v3)
+        {
+            if (v4)
+            {
+                return null;
+            }
+            else
+            {
+                y1 = right.Y2;
+                y2 = left.Y1;
+            }
+        }
+        else
+        {
+            if (v4)
+            {
+                y1 = left.Y1;
+                y2 = right.Y2;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        return new Rectangle(x1, x2, y1, y2);
+    }
+    public bool In(int x, int y)
+    {
+        return x < X2 & x > X1 & y < Y2 & y > Y1;
+    }
+}
+class Graphic
+{
+    public Graphic(WriteableBitmap bitmap)
+    {
+        Bitmap = bitmap;
+    }
+
+    public WriteableBitmap Bitmap { get; }
+    public void DrawRectangle(Rectangle rectangle, byte r, byte g, byte b)
+    {
+        var color = (r << 16) | (g << 8) | b;
+        for (int y = rectangle.Y1; y < rectangle.Y2; y++)
+        {
+            for (int x = rectangle.X1; x < rectangle.X2; x++)
+            {
+                var ptr = Bitmap.BackBuffer + x * 4 + Bitmap.BackBufferStride * y;
+                unsafe
+                {
+                    *((int*)ptr) = color;
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿namespace _3dAppTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Rectangle = global::Rectangle;
+
+public partial class MainWindow : Window
+{
+    private readonly DispatcherTimer _timer = new();
+    private readonly WriteableBitmap _bitmap;
+    private readonly Random _rng = new();
+    private int _f;
+    private Graphic _graphic;
+    public MainWindow()
+    {
+        InitializeComponent();
+        _timer.Interval = TimeSpan.FromSeconds(0.000001);
+        _bitmap = new WriteableBitmap(1000, 1000, 96, 96, PixelFormats.Bgr32, null);
+        image.Source = _bitmap;
+        _graphic = new Graphic(_bitmap);
+        _timer.Tick += Tick;
+        _timer.Start();
+    }
+
+    public void Tick(object? sender, EventArgs args)
+    {
+        _bitmap.Lock();
+        _graphic.DrawRectangle(new Rectangle(100, 200, 100, 200), 255, 0, 0);
+        _bitmap.Unlock();
+        // var rect1 = Rectangle.Create(_rng.Next(900), _rng.Next(900), _rng.Next(100), _rng.Next(100));
+        // var rect2 = Rectangle.Create(_rng.Next(900), _rng.Next(900), _rng.Next(100), _rng.Next(100));
+        // if ((rect1 & rect2) == null)
+        // {
+        //     return;
+        // }
+        // var rect3 = (rect1 & rect2).Value;
+        // _bitmap.Lock();
+        // for (int y = 0; y < _bitmap.PixelWidth; y++)
+        // {
+        //     for (int x = 0; x < _bitmap.PixelHeight; x++)
+        //     {
+        //         var v = (rect1.In(x, y) ? 1 : 0) + (rect2.In(x, y) ? 1 : 0) + (rect3.In(x, y) ? 1 : 0);
+        //         v = v * 80;
+        //         var r = v;
+        //         var g = v;
+        //         var b = v;
+        //         var ptr = _bitmap.BackBuffer + x * 4 + _bitmap.BackBufferStride * y;
+        //         unsafe
+        //         {
+        //             *((int*)ptr) = (r << 16) | (g << 8) | b;
+        //         }
+        //     }
+        // }
+        // _f++;
+        // _bitmap.AddDirtyRect(new(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight));
+        // _bitmap.Unlock();
+    }
+}
